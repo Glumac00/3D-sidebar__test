4ad17a2b531c84a9ee69840813c7e740b4f0cf41
Antun Glumac <antunglumac00@gmail.com>
2023-10-02 13:05:27 +0200

    commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import "./styles/styles.scss"
+
+function App() {
+  return (
+    <>
+      <div>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a  href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a  href="#">Resume</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
